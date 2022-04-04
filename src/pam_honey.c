308a390897b5b876be9185231723bb3962ef7c41
Konstantin Weddige <konstantin@skathi.net>
2022-04-04 21:21:39 +0200

    Implement missing functions

--- src/pam_honey.c
+++ src/pam_honey.c
@@ -8,34 +8,59 @@
 
 int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv)
 {
-        const char *logfile;
-        if (argc >= 1)
-        {
-                logfile = argv[0];
-        }
-        else
-        {
-                return PAM_PERM_DENIED;
-        }
-        int pgu_ret, pga_ret;
-        const char *username = NULL;
-        const char *password = NULL;
-
-        pgu_ret = pam_get_user(pamh, &username, NULL);
-        if (pgu_ret != PAM_SUCCESS || username == NULL)
-        {
-                return PAM_PERM_DENIED;
-        }
-
-        pga_ret = pam_get_authtok(pamh, PAM_AUTHTOK, &password, NULL);
-        if (pga_ret != PAM_SUCCESS || password == NULL || password[0] == '\b')
-        {
-                return PAM_PERM_DENIED;
-        }
-
-        FILE *pFile = fopen(logfile, "a");
-        fprintf(pFile, "%s:%s\n", username, password);
-        fclose(pFile);
-
+    const char *logfile;
+    if (argc >= 1)
+    {
+        logfile = argv[0];
+    }
+    else
+    {
         return PAM_PERM_DENIED;
+    }
+    int pgu_ret, pga_ret;
+    const char *username = NULL;
+    const char *password = NULL;
+
+    pgu_ret = pam_get_user(pamh, &username, NULL);
+    if (pgu_ret != PAM_SUCCESS || username == NULL)
+    {
+        return PAM_PERM_DENIED;
+    }
+
+    pga_ret = pam_get_authtok(pamh, PAM_AUTHTOK, &password, NULL);
+    if (pga_ret != PAM_SUCCESS || password == NULL || password[0] == '\b')
+    {
+        return PAM_PERM_DENIED;
+    }
+
+    FILE *pFile = fopen(logfile, "a");
+    fprintf(pFile, "%s:%s\n", username, password);
+    fclose(pFile);
+
+    return PAM_PERM_DENIED;
+}
+
+int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv)
+{
+    return PAM_IGNORE;
+}
+
+int pam_sm_setcred(pam_handle_t *pamh, int flags, int argc, const char **argv)
+{
+    return PAM_IGNORE;
+}
+
+int pam_sm_open_session(pam_handle_t *pamh, int flags, int argc, const char **argv)
+{
+    return PAM_IGNORE;
+}
+
+int pam_sm_close_session(pam_handle_t *pamh, int flags, int argc, const char **argv)
+{
+    return PAM_IGNORE;
+}
+
+int pam_sm_chauthtok(pam_handle_t *pamh, int flags, int argc, const char **argv)
+{
+    return PAM_IGNORE;
 }
